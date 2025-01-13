15d1b0b6a36ef5e5dc2d9132523f44173f25e61b
lucas halfpenny <lucashalfpenny@gmail.com>
2025-01-13 16:37:35 +0000

    Added bootstrap and popper

--- config/importmap.rb
+++ config/importmap.rb
@@ -14,5 +14,5 @@ pin "@popperjs/core", to: "@popperjs--core.js" # @2.11.8
 # pin "@popperjs/core", to: "@popperjs--core.js" # @2.11.8
 
 # with this:
-pin "bootstrap", to: "bootstrap.min.js", preload: true
-pin "@popperjs/core", to: "popper.js", preload: true
+pin "bootstrap" # @5.3.3
+pin "@popperjs/core", to: "@popperjs--core.js" # @2.11.8
